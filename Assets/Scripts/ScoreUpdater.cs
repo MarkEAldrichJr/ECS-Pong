102fb465a08d85327a547d5edc81c914e8adad23
Mark Aldrich <44414229+MarkEAldrichJr@users.noreply.github.com>
2025-10-20 16:25:06 -0400

    Added TMPro and score updater (should have added last update)

---
+++ Assets/Scripts/ScoreUpdater.cs
@@ -0,0 +1,39 @@
+using Authoring;
+using TMPro;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+public class ScoreUpdater : MonoBehaviour
+{
+    private TextMeshProUGUI _text;
+    private int _scoreLastFrame;
+    
+    private EntityManager _entityManager;
+    private Entity _scoreEntity;
+
+    private void Awake()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+        _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        
+    }
+
+    private void Start()
+    {
+        var scoreEntityQuery = _entityManager.CreateEntityQuery(new EntityQueryBuilder(Allocator.Temp).WithAll<Score>());
+        _scoreEntity = scoreEntityQuery.GetSingletonEntity();
+    }
+
+    private void Update()
+    {
+        var scoreThisFrame = _entityManager.GetComponentData<Score>(_scoreEntity).Value;
+
+        if (scoreThisFrame != _scoreLastFrame)
+        {
+            _text.text = scoreThisFrame.ToString();
+        }
+        
+        _scoreLastFrame = scoreThisFrame;
+    }
+}
